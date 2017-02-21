f008f612c1d156dc926531df4460b3f5954a0d76
Johan Eriksson <jleriksson90@gmail.com>
2017-02-21 18:02:55 +0100

    first changes

--- react-bookmarker/src/App.js
+++ react-bookmarker/src/App.js
@@ -1,12 +1,39 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
+class BookmarkForm extends Component {
 
-    );
+  onsubmit(e){
+    e.preventDefault()
+
+    let newBookmark = {
+      created: Date.now(),
+      name: this.refs.name.value,
+      url: this.refs.url.value
+    }
+    this.props.addBookmark(newBookmark);
+    this.refs.name.value = '';
+    this.refs.url.value = '';
+
+  }
+
+  render (){
+    return (
+      <div className="container jumbotron">
+        <h1>Save your favorite sites!</h1>
+        <form onSubmit={this.onsubmit.bind(this)}>
+          <div>
+          <input type="text" ref="name" className="form__control" required placeholder="Write name here!"/>
+          </div>
+          <div>
+          <input type="text" ref="url" className="form__control" required placeholder="Write URL here!"/>
+          </div>
+          <button type="submit" required className="btn__save">Save</button>
+        </form>
+      </div>
+    )
   }
 }
 
-export default App;
+
+export default BookmarkForm;
--- react-bookmarker/src/index.js
+++ react-bookmarker/src/index.js
@@ -1,9 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import BookmarkForm from './App';
 import './index.css';
 
+
+class BookmarkList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        {"created": 1449445818815, "name": "Facebook", "url":  "http://www.facebook.com" },
+        {"created": 1449445818845, "name": "Reddit", "url": "http://www.reddit.com" }
+      ]};
+  }
+
+  addBookmark (newBookmark) {
+    let newData = this.state.data.concat(newBookmark);
+    this.setState({data:newData});
+  }
+
+  render (){
+    let data = this.state.data;
+    let bookmarkList = data.map((bookmark, index) => {
+      return <li key={index}> <a href={bookmark.url}>{bookmark.name}</a></li>
+    });
+    return (
+      <div>
+      <div className="bookmark__form">
+        <BookmarkForm addBookmark={this.addBookmark.bind(this)} />
+      </div>
+      <div className="bookmark__list">
+        <ul>{bookmarkList}</ul>
+      </div>
+      </div>
+    )
+  }
+}
+
+/*class App extends React.Component {
+  render (){
+    return (
+      <div>
+        <BookmarkList />
+      </div>
+    )
+  }
+}*/
+
 ReactDOM.render(
-  <App />,
+  <BookmarkList />,
   document.getElementById('root')
 );
