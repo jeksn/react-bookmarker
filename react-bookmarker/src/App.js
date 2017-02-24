5a3cc02cbe5d21eb4df5f05c0976a9391934feed
Johan Eriksson <jleriksson90@gmail.com>
2017-02-24 15:00:42 +0100

    second big commit, added remove function and animations

--- react-bookmarker/src/App.js
+++ react-bookmarker/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 class BookmarkForm extends Component {
 
-  onsubmit(e){
+  createBookmark(e){
     e.preventDefault()
 
     let newBookmark = {
@@ -20,13 +20,16 @@ class BookmarkForm extends Component {
   render (){
     return (
       <div className="container jumbotron">
-        <h1>Save your favorite sites!</h1>
-        <form onSubmit={this.onsubmit.bind(this)}>
+        <header>
+          <h1>ReactBookmarker</h1>
+        </header>
+        <h2>Save your favorite sites</h2>
+        <form onSubmit={this.createBookmark.bind(this)}>
           <div>
-          <input type="text" ref="name" className="form__control" required placeholder="Write name here!"/>
+          <input type="text" ref="name" className="form__control" required placeholder="Name of the site" />
           </div>
           <div>
-          <input type="text" ref="url" className="form__control" required placeholder="Write URL here!"/>
+          <input type="text" ref="url" className="form__control" required placeholder="URL, including http://"/>
           </div>
           <button type="submit" required className="btn__save">Save</button>
         </form>
--- react-bookmarker/src/index.js
+++ react-bookmarker/src/index.js
@@ -1,28 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import BookmarkForm from './App';
-import './index.css';
-
+import './App.css';
 
 class BookmarkList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: [
-        {"created": 1449445818815, "name": "Facebook", "url":  "http://www.facebook.com" },
-        {"created": 1449445818845, "name": "Reddit", "url": "http://www.reddit.com" }
-      ]};
+      data: []};
   }
+
+  getInitialState() {
+    var selectedOption = JSON.parse(localStorage.getItem( "data" ));
+    return {
+        selectedOption: selectedOption
+    };
+  }
+
 
   addBookmark (newBookmark) {
     let newData = this.state.data.concat(newBookmark);
     this.setState({data:newData});
   }
+  //Adds state to LocalStorage
+  componentDidUpdate (prevProps, prevState) {
+    localStorage.state = JSON.stringify(this.state);
+  }
+
+  deleteBookmark (e) {
+    var array = this.state.data;
+    var index = array.indexOf(e.target.value)
+    array.splice(index, 1);
+    this.setState({data:array});
+
+  }
 
   render (){
     let data = this.state.data;
     let bookmarkList = data.map((bookmark, index) => {
-      return <li key={index}> <a href={bookmark.url}>{bookmark.name}</a></li>
+      return <li key={index} className="link__active-fade">
+                <a href={bookmark.url} target="_blank">{bookmark.name}</a>
+                <span className="delete" onClick={this.deleteBookmark.bind(this)}>✖</span>
+              </li>
     });
     return (
       <div>
@@ -36,16 +55,6 @@ class BookmarkList extends React.Component {
     )
   }
 }
-
-/*class App extends React.Component {
-  render (){
-    return (
-      <div>
-        <BookmarkList />
-      </div>
-    )
-  }
-}*/
 
 ReactDOM.render(
   <BookmarkList />,
